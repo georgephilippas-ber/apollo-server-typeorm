a70ff1c2293f0e639725363c28b1344a0ce97591
georgephilippas-ber <george.philippas@outlook.com.gr>
2022-07-06 18:58:30 +0200

    Initial commit

--- database/database-model/features/product/manager/product-manager.ts
+++ database/database-model/features/product/manager/product-manager.ts
@@ -1,7 +1,7 @@
 import {DatabaseProvider} from "../../../../database-provider";
 import {Product, ProductCategory} from "../schemas/product-schema";
 import {ProductPhoto} from "../schemas/product-photo-schema";
-import {ILike} from "typeorm";
+import {ILike, In} from "typeorm";
 
 export class ProductManager
 {
@@ -71,12 +71,20 @@ export class ProductManager
                 category_name: ILike("%" + partial_category_name_ + "%")
             }
 
-        })
+        });
     }
 
     async productByCategoryName(category_name: string): Promise<Product[]>
     {
-        return this.databaseProvider.getDataSource().getRepository(Product).createQueryBuilder("p").leftJoinAndSelect("p.categories", "pc").where("pc.category_name = :category_name", {category_name}).getMany();
+        //lazy
+        let id_array_ = (await this.databaseProvider.getDataSource().getRepository(Product).createQueryBuilder("p").leftJoinAndSelect("p.categories", "pc").where("pc.category_name = :category_name", {category_name}).getMany()).map(value => value.id);
+
+        //eager
+        return this.databaseProvider.getDataSource().getRepository(Product).find({
+            where: {
+                id: In(id_array_)
+            }
+        });
     }
 
     async all(): Promise<Product[]>
