c75e808ef955872708d2a14eff8e870cc7ec0d8a
georgephilippas-ber <george.philippas@outlook.com.gr>
2022-07-07 13:52:07 +0200

    Initial commit

--- core/units/units.ts
+++ core/units/units.ts
@@ -1,10 +1,10 @@
 import {unit, Unit} from "mathjs";
 
-function convert(product_quantity_type: "mass" | "volume", target_unit_: string, target_quantity_: number, object_: any): typeof object_
+function convert(product_quantity_type: "mass" | "volume", target_units_: string, target_quantity_: number, object_: any, recursive_: boolean = true): typeof object_
 {
     let record_units_: Unit = product_quantity_type === "mass" ? unit(100., "g") : unit(100., "ml");
 
-    let scale_: number = record_units_.to(target_unit_).toNumber();
+    let scale_: number = record_units_.to(target_units_).toNumber();
 
     let converted_object_: any = {};
 
@@ -14,8 +14,8 @@ function convert(product_quantity_type: "mass" | "volume", target_unit_: string,
 
         if (typeof value_ === "number")
             converted_object_[property_ as keyof typeof converted_object_] = value_ / scale_ * target_quantity_;
-        else if (typeof value_ === "object")
-            converted_object_[property_ as keyof typeof converted_object_] = convert(product_quantity_type, target_unit_, target_quantity_, value_);
+        else if ((typeof value_ === "object") && recursive_)
+            converted_object_[property_ as keyof typeof converted_object_] = convert(product_quantity_type, target_units_, target_quantity_, value_);
         else
             converted_object_[property_] = value_;
     }
@@ -23,8 +23,38 @@ function convert(product_quantity_type: "mass" | "volume", target_unit_: string,
     return converted_object_ as typeof object_;
 }
 
-let sample: any = {
-    riboflavin: 20
+function presentable(object_: any, units_: string, recursive_: boolean = false): typeof object_
+{
+    let presentable_object_: any = {};
+
+    for (let property_ in object_)
+    {
+        let value_ = object_[property_ as keyof typeof object_];
+
+        if (typeof value_ === "number")
+            presentable_object_[property_ as keyof typeof presentable_object_] = unit(value_, units_).format({
+                notation: "auto",
+                precision: 2
+            });
+        else if ((typeof value_ === "object") && recursive_)
+            presentable_object_[property_ as keyof typeof presentable_object_] = presentable(value_, units_);
+        else
+            presentable_object_[property_] = value_;
+    }
+
+    return presentable_object_;
 }
 
-console.log(convert("mass", "g", 200, sample));
+let sample: any = {
+    riboflavin: 20,
+    f: "sfsdl",
+    g: {
+        l: "nothing",
+        p: 10
+    }
+}
+
+
+console.log(convert("mass", "mg", 200, sample, true));
+
+console.log(presentable(sample, "mg", false));
